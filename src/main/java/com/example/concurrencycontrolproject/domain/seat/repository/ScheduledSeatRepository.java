d522234c846eec8deeae4e5de9752b8e95dc7e8f
Yujun <pathfinder357@github.com>
2025-03-25 17:51:47 +0900

    feat: seat 기능작업
    # Conflicts:
    #	build.gradle

---
+++ src/main/java/com/example/concurrencycontrolproject/domain/seat/repository/ScheduledSeatRepository.java
@@ -0,0 +1,8 @@
+package com.example.concurrencycontrolproject.domain.seat.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.concurrencycontrolproject.domain.seat.entity.Seat;
+
+public interface ScheduledSeatRepository extends JpaRepository<Seat, Long> {
+}
