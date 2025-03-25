5492961802890ef62e6664ea31d806270453c308
uyr83157 <uyr83157@gmail.com>
2025-03-25 23:26:15 +0900

    Feat: 티켓 기본 구조 구현

---
+++ src/main/java/com/example/concurrencycontrolproject/domain/ticket/dto/response/TicketResponseDto.java
@@ -0,0 +1,40 @@
+package com.example.concurrencycontrolproject.domain.ticket.dto.response;
+
+import java.time.LocalDateTime;
+
+import com.example.concurrencycontrolproject.domain.seat.dto.response.SeatResponseDto;
+import com.example.concurrencycontrolproject.domain.ticket.entity.Ticket;
+import com.example.concurrencycontrolproject.domain.ticket.entity.TicketStatus;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class TicketResponseDto {
+
+	private Long id;
+	private Long scheduleId;
+	private TicketStatus status;
+	private LocalDateTime createdAt;
+	private LocalDateTime modifiedAt;
+	private SeatResponseDto seat;
+
+	public static TicketResponseDto ticketResponseDto(Ticket ticket) {
+		return TicketResponseDto.builder()
+			.id(ticket.getId())
+			.scheduleId(ticket.getScheduleSeat().getSchedule().getId())
+			.status(ticket.getStatus())
+			.createdAt(ticket.getCreatedAt())
+			.modifiedAt(ticket.getModifiedAt())
+			.seat(SeatResponseDto.builder()  // SeatResponse 생성
+				.id(ticket.getScheduleSeat().getSeat().getId())
+				.number(ticket.getScheduleSeat().getSeat().getNumber())
+				.grade(ticket.getScheduleSeat().getSeat().getGrade())
+				.price(ticket.getScheduleSeat().getSeat().getPrice())
+				.section(ticket.getScheduleSeat().getSeat().getSection())
+				.build())
+			.build();
+	}
+
+}
--- src/main/java/com/example/concurrencycontrolproject/domain/ticket/entity/Ticket.java
+++ src/main/java/com/example/concurrencycontrolproject/domain/ticket/entity/Ticket.java
@@ -1,4 +1,56 @@
 package com.example.concurrencycontrolproject.domain.ticket.entity;
 
-public class Ticket {
+import com.example.concurrencycontrolproject.domain.common.entity.Timestamped;
+import com.example.concurrencycontrolproject.domain.scheduleSeat.entity.ScheduleSeat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "tickets")
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Ticket extends Timestamped {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "schedule_seat_id", nullable = false)
+	private ScheduleSeat scheduleSeat;
+
+	@Enumerated(EnumType.STRING)
+	private TicketStatus status;
+
+	// 정적 팩토리 메서드
+	public static Ticket saveTicket(ScheduleSeat scheduleSeat) {
+		return Ticket.builder()
+			.scheduleSeat(scheduleSeat)
+			.status(TicketStatus.RESERVED) // 기본값
+			.build();
+	}
+
+	// 인스턴스 메서드
+	public void cancel() {
+		if (this.status != TicketStatus.RESERVED) {
+			throw new IllegalStateException("취소할 수 없습니다.");
+		}
+		this.status = TicketStatus.CANCELED;
+	}
 }
