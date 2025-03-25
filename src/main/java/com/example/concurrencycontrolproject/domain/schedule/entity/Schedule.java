5b7d526292a3a30fe66eca359c35acbbcabe3070
qwerdfJIN <test19980224@gmail.com>
2025-03-25 11:26:36 +0900

    feat: 공연 스케줄 기본 구조 작성
    - Schedule Entity 및 ScheduleStatus enum 작성
    - Controller, Service, Dto, Entity, enums, Repository 기본 틀 생성
    - 패키지 구조 및 연관 관계 매핑 완료
    - 상태 수정과 스케줄 정보 수정 분리 설계 적용

--- src/main/java/com/example/concurrencycontrolproject/domain/schedule/entity/Schedule.java
+++ src/main/java/com/example/concurrencycontrolproject/domain/schedule/entity/Schedule.java
@@ -1,4 +1,56 @@
 package com.example.concurrencycontrolproject.domain.schedule.entity;
 
-public class Schedule {
+import static lombok.AccessLevel.*;
+
+import java.time.LocalDateTime;
+
+import com.example.concurrencycontrolproject.domain.concert.entity.Concert;
+import com.example.concurrencycontrolproject.domain.schedule.enums.ScheduleStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Table(name = "schedules")
+public class Schedule extends BaseTime {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManytoOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "concert_id", nullable = false)
+	private Concert concert;
+
+	private LocalDateTime datetime;
+
+	@Enumerated(EnumType.STRING)
+	private ScheduleStatus status;
+
+	private LocalDateTime deletedAt;
+
+	public Schedule(Concert concert, LocalDateTime datetime, ScheduleStatus status) {
+		this.concert = concert;
+		this.datetime = datetime;
+		this.status = status;
+	}
+
+	public void updateDateTime(LocalDateTime datetime) {
+		this.datetime = datetime;
+	}
+
+	public void updateStatus(ScheduleStatus status) {
+		this.status = status;
+	}
 }
