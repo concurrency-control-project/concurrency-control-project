4568731e3715d4b25c687e236d321a00dabf9488
uyr83157 <uyr83157@gmail.com>
2025-03-27 17:10:32 +0900

    feat: 티켓 소유 로직 추가, DTO 컨벤션에 맞게 수정, 쿼리DSL 로직 수정, 좌석 상태 변경 로직 수정

--- src/main/java/com/example/concurrencycontrolproject/domain/ticket/dto/response/TicketResponse.java
+++ src/main/java/com/example/concurrencycontrolproject/domain/ticket/dto/response/TicketResponse.java
@@ -2,32 +2,34 @@ package com.example.concurrencycontrolproject.domain.ticket.dto.response;
 
 import java.time.LocalDateTime;
 
-import com.example.concurrencycontrolproject.domain.seat.dto.response.SeatResponseDto;
+import com.example.concurrencycontrolproject.domain.seat.dto.response.SeatResponse;
 import com.example.concurrencycontrolproject.domain.ticket.entity.Ticket;
 import com.example.concurrencycontrolproject.domain.ticket.entity.TicketStatus;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
-public class TicketResponseDto {
+@AllArgsConstructor
+public class TicketResponse {
 
 	private Long id;
 	private Long scheduleId;
 	private TicketStatus status;
 	private LocalDateTime createdAt;
 	private LocalDateTime modifiedAt;
-	private SeatResponseDto seat;
+	private SeatResponse seat;
 
-	public static TicketResponseDto ticketResponseDto(Ticket ticket) {
-		return TicketResponseDto.builder()
+	public static TicketResponse ticketResponse(Ticket ticket) {
+		return TicketResponse.builder()
 			.id(ticket.getId())
 			.scheduleId(ticket.getScheduleSeat().getSchedule().getId())
 			.status(ticket.getStatus())
 			.createdAt(ticket.getCreatedAt())
 			.modifiedAt(ticket.getModifiedAt())
-			.seat(SeatResponseDto.builder()  // SeatResponse 생성
+			.seat(SeatResponse.builder()  // SeatResponse 생성
 				.id(ticket.getScheduleSeat().getSeat().getId())
 				.number(ticket.getScheduleSeat().getSeat().getNumber())
 				.grade(ticket.getScheduleSeat().getSeat().getGrade())
--- src/main/java/com/example/concurrencycontrolproject/domain/ticket/entity/Ticket.java
+++ src/main/java/com/example/concurrencycontrolproject/domain/ticket/entity/Ticket.java
@@ -2,6 +2,8 @@ package com.example.concurrencycontrolproject.domain.ticket.entity;
 
 import com.example.concurrencycontrolproject.domain.common.entity.Timestamped;
 import com.example.concurrencycontrolproject.domain.scheduleSeat.entity.ScheduleSeat;
+import com.example.concurrencycontrolproject.domain.ticket.exception.TicketErrorCode;
+import com.example.concurrencycontrolproject.domain.ticket.exception.TicketException;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -49,7 +51,7 @@ public class Ticket extends Timestamped {
 	// 티켓 취소 인스턴스 메서드
 	public void cancel() {
 		if (this.status != TicketStatus.RESERVED) {
-			throw new IllegalStateException("취소할 수 없습니다.");
+			throw new TicketException(TicketErrorCode.TICKET_UPDATE_INVALID_STATUS);
 		}
 		this.status = TicketStatus.CANCELED;
 	}
@@ -59,6 +61,15 @@ public class Ticket extends Timestamped {
 		if (this.status == TicketStatus.RESERVED) {
 			this.status = TicketStatus.EXPIRED;
 		}
+	}
+
+	// 좌석 변경
+	public void changeScheduleSeat(ScheduleSeat newScheduleSeat) {
+		if (newScheduleSeat == null) {
+			throw new TicketException(TicketErrorCode.SCHEDULE_SEAT_BAD_REQUEST);
+		}
+		this.scheduleSeat = newScheduleSeat;
+
 	}
 
 }
