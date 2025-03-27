7b7b347a1cd29a78489b01f3c65ac5136fc8b0b3
Yujun <pathfinder357@github.com>
2025-03-27 22:21:13 +0900

    ㅇㅇ

--- src/test/java/com/example/concurrencycontrolproject/global/redis/RedisCacheUtilTest.java
+++ src/test/java/com/example/concurrencycontrolproject/global/redis/RedisCacheUtilTest.java
@@ -36,6 +36,24 @@ class RedisCacheUtilTest {
 		refreshToken = new RefreshToken(userId, token);
 	}
 
+	RefreshToken refreshToken;
+	String token = "token";
+	String userId = "1";
+
+	@BeforeEach
+	void setUp() {
+		refreshToken = new RefreshToken(userId, token);
+	}
+
+	RefreshToken refreshToken;
+	String token = "token";
+	String userId = "1";
+
+	@BeforeEach
+	void setUp() {
+		refreshToken = new RefreshToken(userId, token);
+	}
+
 	@Test
 	void saveRefreshToken() {
 		String save = redisCacheUtil.saveRefreshToken(token, userId);
