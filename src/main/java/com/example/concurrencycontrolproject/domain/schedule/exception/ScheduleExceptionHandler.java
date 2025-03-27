085348550598f62ee22059d4251421e0f1e529bc
qwerdfJIN <test19980224@gmail.com>
2025-03-27 19:14:40 +0900

    chore: ScheduleExceptionHandler로 예외 처리 관련 주석 이동

--- src/main/java/com/example/concurrencycontrolproject/domain/schedule/exception/ScheduleExceptionHandler.java
+++ src/main/java/com/example/concurrencycontrolproject/domain/schedule/exception/ScheduleExceptionHandler.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+// GlobalExceptionHandler와의 충돌을 피하기 위해 스케줄 도메인 전용 핸들러를 별도로 분리했습니다.
+// 해당 예외 처리 로직은 추후 통합할 예정입니다.
 @RestControllerAdvice(basePackages = "com.example.concurrencycontrolproject.domain.schedule")
 public class ScheduleExceptionHandler {
 
