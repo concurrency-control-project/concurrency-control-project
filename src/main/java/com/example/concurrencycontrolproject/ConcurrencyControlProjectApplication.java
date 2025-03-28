a3d041a85c8b5aaa435673dbdd1e3ecc0c1a05ca
uyr83157 <uyr83157@gmail.com>
2025-03-29 02:21:57 +0900

    feat: 스케줄시트 연관관계 해제, 스케줄 + 시트 각각 따로 참조, 분산락 리팩터링 및 DB Unique 제약 조건 추가, 테스트 코드 완료
    # Conflicts:
    #	src/main/java/com/example/concurrencycontrolproject/domain/schedule/entity/Schedule.java
    #	src/main/java/com/example/concurrencycontrolproject/domain/schedule/enums/ScheduleStatus.java
    #	src/main/java/com/example/concurrencycontrolproject/domain/schedule/repository/ScheduleRepository.java

--- src/main/java/com/example/concurrencycontrolproject/ConcurrencyControlProjectApplication.java
+++ src/main/java/com/example/concurrencycontrolproject/ConcurrencyControlProjectApplication.java
@@ -14,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableScheduling
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+// @EnableRedisRepositories(basePackages = "com.example.concurrencycontrolproject.domain.seat.entity.scheduledSeat")
 public class ConcurrencyControlProjectApplication {
 
 	public static void main(String[] args) {
